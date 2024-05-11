e7e51f4bb001999acbaec5f724972230934236b2
sshuy <sshuy@protonmail.ch>
2024-05-11 01:05:16 +0100

    printing out amount of words in book and refactor into more functions

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
+    book_path = "books/frankenstein.txt"
+    file_contents = get_book_data(book)
+    words = count_words(file_contents)
+    print(f"{words} words were found in the book!")
+
+def get_book_data(book_path):
+    with open(book_path) as f:
+        return f.read()
+
+def count_words(book_text):
+    words = book_text.split()
+    return len(words)
 
 main()
