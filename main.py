5c2ad2cdd0b77836fd79565a436af78bec885f94
sshuy <sshuy@protonmail.ch>
2024-05-11 03:01:06 +0100

    feat: reports the most common characters in the book in a sorted manner

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ def main():
     book_path = "books/frankenstein.txt"
     file_contents = get_book_data(book_path)
     words = count_words(file_contents)
+
+    print(f"--- Begin report for {book_path} ---")
     print(f"{words} words were found in the book!")
-    total_count = count_letters(file_contents)
-    print(total_count)
+
+    letters_in_novel = count_letters(file_contents)
+    list_data = convert_dict_to_list(letters_in_novel)
+    list_data.sort(reverse=True, key=sort_it)
+    for data in list_data:
+        print(f"The '{data['letter']}' character was found {data['num']} times")
+    print("--- End Report ---")
+
 
 def get_book_data(book_path):
     with open(book_path) as f:
@@ -14,12 +22,23 @@ def count_words(book_text):
     words = book_text.split()
     return len(words)
 
+def convert_dict_to_list(dict):
+    list = []
+    for char in dict:
+        num = dict[char]
+        next = {"letter": char, "num": num}
+        list.append(next)
+    return list
+
+def sort_it(dict):
+    return dict['num']
+
 def count_letters(book_text):
     lowercase_text = book_text.lower()
     letter_count = {}
 
     for char in lowercase_text:
-        if not char in letter_count:
+        if char.isalpha() and not char in letter_count:
             letter_count[char] = 0
         if char in letter_count:
             count = letter_count[char]
